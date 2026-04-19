4e9e0bc2d2f28552788b5f7ba037ed08500d5ab8
seyebirane95 <92035274+seyebirane95@users.noreply.github.com>
2026-04-19 20:05:36 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'  # ici les images uploadées
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ------------------------------
 # AUTRES
