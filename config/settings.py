eb0bdc37472e3ce490f6e048dc73312ca01a69d5
seyebirane95 <92035274+seyebirane95@users.noreply.github.com>
2026-04-20 13:57:38 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -105,9 +105,9 @@ USE_TZ = True
 # STATIC & MEDIA FILES
 # ------------------------------
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 # ------------------------------
 import cloudinary
 import cloudinary.uploader
