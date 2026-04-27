6950b0c317343f31751c23ecfefd418015adbf15
seyebirane95 <92035274+seyebirane95@users.noreply.github.com>
2026-04-27 15:24:43 +0200

    Remove duplicate import in urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from orders.views import pay_on_delivery, pay_on_livraison, success, path
+from orders.views import pay_on_delivery, pay_on_livraison, success
 
 # Personnalisation dynamique de l'admin
 admin.site.site_header = getattr(settings, "ADMIN_SITE_HEADER", "Django Administration")
