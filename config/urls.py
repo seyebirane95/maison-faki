ffe0ac4c4d364f0a54a8577350893dcc95240a94
seyebirane95 <92035274+seyebirane95@users.noreply.github.com>
2026-04-27 19:21:26 +0200

    Add health check endpoint to urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from orders.views import pay_on_delivery, pay_on_livraison, success
+from orders.views import pay_on_delivery, pay_on_livraison, success,health
 
 # Personnalisation dynamique de l'admin
 admin.site.site_header = getattr(settings, "ADMIN_SITE_HEADER", "Django Administration")
@@ -16,7 +16,8 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
     path('cart/pay-on-delivery/', pay_on_delivery, name='pay_on_delivery'),
-    path('cart/pay-on-livraison/', pay_on_livraison, name='pay_on_livraison'),   
+    path('cart/pay-on-livraison/', pay_on_livraison, name='pay_on_livraison'),
+    path('health/', health, name='health'),
 ]
 
 # Servir les fichiers statiques et médias en développement
