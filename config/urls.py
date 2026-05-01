e9a645bcd91a408844d3cc6725633291ca3a8f81
seyebirane95 <92035274+seyebirane95@users.noreply.github.com>
2026-05-01 09:35:14 +0200

    Add robots.txt route to URL configuration

--- config/urls.py
+++ config/urls.py
@@ -2,15 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from orders.views import pay_on_delivery, pay_on_livraison, success,health
+from orders.views import pay_on_delivery, pay_on_livraison, success,health,robots_txt
 
 # Personnalisation dynamique de l'admin
 admin.site.site_header = getattr(settings, "ADMIN_SITE_HEADER", "Django Administration")
 admin.site.site_title = getattr(settings, "ADMIN_SITE_TITLE", "Admin")
 admin.site.index_title = getattr(settings, "ADMIN_INDEX_TITLE", "Bienvenue")
+def robots_txt(request):
+    content = "User-agent: *\nAllow: /\nSitemap: https://www.maisonfaki.com/sitemap.xml"
+    return HttpResponse(content, content_type="text/plain")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robots.txt', robots_txt),
     path('', include('core.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
